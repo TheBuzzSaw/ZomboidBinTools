e6e4d08c91ac896d0be228203be9f87d4069aa2b
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2014-09-12 12:36:36 -0600

    Reading the file piece by piece.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,118 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
 using namespace std;
+
+vector<uint8_t> LoadFile(const char* path)
+{
+    vector<uint8_t> result;
+
+    if (path && *path)
+    {
+        ifstream fin(path, ifstream::binary);
+
+        if (fin)
+        {
+            fin.seekg(0, ios::end);
+            result.resize(fin.tellg());
+            fin.seekg(0, ios::beg);
+            fin.read((char*)result.data(), result.size());
+            fin.close();
+        }
+    }
+
+    return move(result);
+}
+
+template<typename T>
+inline const T Get(const void* target)
+{
+    return *reinterpret_cast<const T*>(target);
+}
+
+template<typename T>
+const T Read(const uint8_t*& target)
+{
+    const T result = Get<T>(target);
+    target += sizeof(T);
+    return result;
+}
+
+template<typename T>
+const T EndianSwap(const T value)
+{
+    const uint8_t* oldBytes = reinterpret_cast<const uint8_t*>(&value);
+    uint8_t newBytes[sizeof(T)];
+
+    const size_t LastIndex = sizeof(T) - 1;
+    for (size_t i = 0; i < sizeof(T); ++i)
+        newBytes[i] = oldBytes[LastIndex - i];
+
+    return *reinterpret_cast<const T*>(newBytes);
+}
+
+void Dump(const vector<uint8_t>& data, ostream& out)
+{
+    const char* worldLabels[] = {
+        "WorldX",
+        "WorldY",
+        "WorldXa",
+        "WorldYa",
+        "WorldZa"
+    };
+
+    const uint8_t* current = data.data();
+    for (int i = 0; i < 5; ++i)
+    {
+        out << worldLabels[i]
+            << " : "
+            << EndianSwap(Read<int>(current))
+            << '\n';
+    }
+
+    current += 5; // unknown block
+
+    const char* positionLabels[] = {
+        "x offset",
+        "y offset",
+        "X",
+        "Y",
+        "Z"
+    };
+
+    for (int i = 0; i < 5; ++i)
+    {
+        out << positionLabels[i]
+            << " : "
+            << Read<float>(current)
+            << '\n';
+    }
+
+    out << "Player Direction : " << EndianSwap(Read<int>(current)) << '\n';
+}
 
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+    {
+        cerr << "Y U NO SPECIFY INPUT FILES\n";
+        return 1;
+    }
+
+    for (int i = 1; i < argc; ++i)
+    {
+        vector<uint8_t> data = LoadFile(argv[i]);
+
+        if (data.size() > 0)
+        {
+            Dump(data, cout);
+        }
+        else
+        {
+            cerr << "failed: " << argv[i] << '\n';
+        }
+    }
+
     return 0;
 }
